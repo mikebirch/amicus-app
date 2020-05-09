cf6b6e29c1d1dddbaa53e5ab4ab4d61b987816a4
Michael Birch <mbirch321@gmail.com>
2020-05-09 15:05:31 +1200

    Add sql file

---
+++ database/amicus.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `blog_posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `summary` mediumtext COLLATE utf8mb4_unicode_ci,
+  `body` longtext COLLATE utf8mb4_unicode_ci,
+  `published` tinyint(4) DEFAULT '1',
+  `sticky` tinyint(4) NOT NULL DEFAULT '0',
+  `in_rss` tinyint(4) NOT NULL DEFAULT '1',
+  `meta_title` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `meta_description` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `author` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  UNIQUE KEY `slug` (`slug`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `published` tinyint(4) DEFAULT '1',
+  `meta_title` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `meta_description` text COLLATE utf8mb4_unicode_ci,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
