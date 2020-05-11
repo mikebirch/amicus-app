cd1bab1955db8e3d40ee96c70fa660fb1de13969
Michael Birch <mbirch321@gmail.com>
2020-05-11 22:28:24 +1200

    Check if function exists

--- src/Utility/debug.php
+++ src/Utility/debug.php
@@ -1,10 +1,12 @@
 <?php
-function debug($var)
-{
-    $bt = debug_backtrace();
-    $caller = array_shift($bt);
-    echo '<strong>debug() called  in ' . $caller['file']. ' at line ' . $caller['line'] . '</strong>';
-    echo '<pre>';
-    var_dump($var);
-    echo '</pre>';
+if (!function_exists('debug')) {
+    function debug($var)
+    {
+        $bt = debug_backtrace();
+        $caller = array_shift($bt);
+        echo '<strong>debug() called  in ' . $caller['file']. ' at line ' . $caller['line'] . '</strong>';
+        echo '<pre>';
+        var_dump($var);
+        echo '</pre>';
+    }
 }
