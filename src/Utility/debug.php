7ae1bf156d1317f07a7fa2f7957435662d0aa90b
Michael Birch <mbirch321@gmail.com>
2020-05-13 21:34:01 +1200

    Restrict debug with config

--- src/Utility/debug.php
+++ src/Utility/debug.php
@@ -1,5 +1,14 @@
 <?php
-if (!function_exists('debug')) {
+use Showus\Configure\Configure;
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+define("ROOT", dirname(__DIR__, 2));
+define("CONFIG", ROOT . DS . 'config');
+define("APP", ROOT . DS . 'src');
+
+
+if ( !function_exists('debug') ) {
     /**
      * debug variables
      *
@@ -8,11 +17,16 @@ if (!function_exists('debug')) {
      */
     function debug($var)
     {
-        $bt = debug_backtrace();
-        $caller = array_shift($bt);
-        echo '<strong>debug() called  in ' . $caller['file']. ' at line ' . $caller['line'] . '</strong>';
-        echo '<pre>';
-        var_dump($var);
-        echo '</pre>';
+        $config = Configure::read();
+        if ( $config['environment'] != 'prod' && $config['debug'] == true ) {
+            $bt = debug_backtrace();
+            $caller = array_shift($bt);
+            echo '<strong>debug() called  in ' . $caller['file']. ' at line ' . $caller['line'] . '</strong>';
+            echo '<pre>';
+            var_dump($var);
+            echo '</pre>';
+        } else {
+            return true;
+        }
     }
 }
