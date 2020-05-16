64cac8e47a443a3a0d2d868a141e4d5ef17e1076
Michael Birch <mbirch321@gmail.com>
2020-05-16 23:30:24 +1200

    Update db sql

--- database/amicus.sql
+++ database/amicus.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS `blog_posts`;
 CREATE TABLE `blog_posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `summary` mediumtext COLLATE utf8mb4_unicode_ci,
   `body` longtext COLLATE utf8mb4_unicode_ci,
+  `tags` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Please only use letters, numerals and dashes.',
   `published` tinyint(4) DEFAULT '1',
   `sticky` tinyint(4) NOT NULL DEFAULT '0',
   `in_rss` tinyint(4) NOT NULL DEFAULT '1',
@@ -16,14 +18,27 @@ CREATE TABLE `blog_posts` (
   UNIQUE KEY `slug` (`slug`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+DROP TABLE IF EXISTS `main_menu`;
+CREATE TABLE `main_menu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `url` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sort` int(10) unsigned DEFAULT NULL COMMENT 'Choose the order that items show in the menu',
+  `published` tinyint(4) DEFAULT '1',
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `pages`;
 CREATE TABLE `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `published` tinyint(4) DEFAULT '1',
   `meta_title` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  `meta_description` text COLLATE utf8mb4_unicode_ci,
+  `meta_description` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
