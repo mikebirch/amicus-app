c16729dc8ee4719f67d1b363b0fee3791e4ddf7b
Michael Birch <mbirch321@gmail.com>
2020-05-17 21:24:23 +1200

    Update example config file

--- config/config_example.php
+++ config/config_example.php
@@ -2,6 +2,7 @@
 return [
     'environment' => 'dev', // either dev or prod
     'debug' => true, // true or false
+    'show_debug_bar' => true, // true or false
     'Datasources' => [
         // indices match environment
         'dev' => [
@@ -21,24 +22,32 @@ return [
             'charset' => 'utf8mb4'
         ]
     ],
+    'Cache' => [
+        'expiration' => [
+            'long' => 365*24*60*60, // one year
+            'short' => 12*60*60 // 12 hours
+        ]
+    ],
     'paths' => [
         'Cache' => ROOT . DS . 'cache',
         'Log' => ROOT . DS . 'logs',
         'Template' => APP . DS . 'Template'
     ],    
     'blog' => [
-        'meta_description' => '',
         'number_latest_posts' => 3,
+        'posts_per_page' => 10,
         'show_summary_on_index' => true,
         'show_date_on_index' => false,
         'date_format_on_index' => 'j M Y',
         'date_format_on_view' => 'j F Y',
-        'use_discus' => true,
+        'display_reading_times' => true,
+        'display_tags_on_view' => true,
+        'use_discus' => false,
         'disqus_developer_mode' => false
     ],
     'site' => [
         'title' => 'Site title',
-        'meta_description' => ''
+        'meta_description' => 'Site Description'
     ],
     'google_analytics' => true
 ];
